7ccab3b8505b68372faa3cbfd8292121c4b367a5
Sina <sina.alsharif@gmail.com>
2021-07-23 00:16:22 +1000

    Change configs and data

--- lassonet/__init__.py
+++ lassonet/__init__.py
@@ -1,4 +1,4 @@
 from .model import LassoNet
 from .prox import prox
-from .interfaces import LassoNetClassifier, LassoNetRegressor, lassonet_path
-from .utils import plot_path
+from .interfaces import LassoNetClassifier, BaseLassoNet, LassoNetRegressor, lassonet_path
+from .utils import plot_path, plot_importance
